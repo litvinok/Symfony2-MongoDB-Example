e21c2a3de81ce357326052f7e1fab93150fa647c
Alex Litvinok <litvinok@gmail.com>
2013-06-03 13:28:58 +0300

    fix translation

--- src/Mongo/ExampleBundle/Document/Category.php
+++ src/Mongo/ExampleBundle/Document/Category.php
@@ -106,4 +106,338 @@ class Category
         $this->children = new ArrayCollection();
         $this->translations = new ArrayCollection();
     }
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return self
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string $title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return self
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string $description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return self
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string $slug
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set lft
+     *
+     * @param int $lft
+     * @return self
+     */
+    public function setLft($lft)
+    {
+        $this->lft = $lft;
+        return $this;
+    }
+
+    /**
+     * Get lft
+     *
+     * @return int $lft
+     */
+    public function getLft()
+    {
+        return $this->lft;
+    }
+
+    /**
+     * Set rgt
+     *
+     * @param int $rgt
+     * @return self
+     */
+    public function setRgt($rgt)
+    {
+        $this->rgt = $rgt;
+        return $this;
+    }
+
+    /**
+     * Get rgt
+     *
+     * @return int $rgt
+     */
+    public function getRgt()
+    {
+        return $this->rgt;
+    }
+
+    /**
+     * Set parent
+     *
+     * @param Mongo\ExampleBundle\Document\Category $parent
+     * @return self
+     */
+    public function setParent(\Mongo\ExampleBundle\Document\Category $parent)
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return Mongo\ExampleBundle\Document\Category $parent
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Set root
+     *
+     * @param int $root
+     * @return self
+     */
+    public function setRoot($root)
+    {
+        $this->root = $root;
+        return $this;
+    }
+
+    /**
+     * Get root
+     *
+     * @return int $root
+     */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+     * Set level
+     *
+     * @param int $level
+     * @return self
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return int $level
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Add children
+     *
+     * @param Mongo\ExampleBundle\Document\Category $children
+     */
+    public function addChildren(\Mongo\ExampleBundle\Document\Category $children)
+    {
+        $this->children[] = $children;
+    }
+
+    /**
+    * Remove children
+    *
+    * @param <variableType$children
+    */
+    public function removeChildren(\Mongo\ExampleBundle\Document\Category $children)
+    {
+        $this->children->removeElement($children);
+    }
+
+    /**
+     * Get children
+     *
+     * @return Doctrine\Common\Collections\Collection $children
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * Set created
+     *
+     * @param date $created
+     * @return self
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return date $created
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param date $updated
+     * @return self
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return date $updated
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param string $createdBy
+     * @return self
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return string $createdBy
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param string $updatedBy
+     * @return self
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return string $updatedBy
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * Add translations
+     *
+     * @param Mongo\ExampleBundle\Document\CategoryTranslation $translations
+     */
+    public function addTranslation(\Mongo\ExampleBundle\Document\CategoryTranslation $translations)
+    {
+        $this->translations[] = $translations;
+    }
+
+    /**
+    * Remove translations
+    *
+    * @param <variableType$translations
+    */
+    public function removeTranslation(\Mongo\ExampleBundle\Document\CategoryTranslation $translations)
+    {
+        $this->translations->removeElement($translations);
+    }
+
+    /**
+     * Get translations
+     *
+     * @return Doctrine\Common\Collections\Collection $translations
+     */
+    public function getTranslations()
+    {
+        return $this->translations;
+    }
 }
--- src/Mongo/ExampleBundle/Document/CategoryTranslation.php
+++ src/Mongo/ExampleBundle/Document/CategoryTranslation.php
@@ -3,12 +3,12 @@
 namespace Mongo\ExampleBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
-use Gedmo\Translatable\Document\MappedSuperclass\AbstractPersonalTranslation;
+use Gedmo\Translatable\Document\MappedSuperclass\AbstractTranslation;
 
 /**
  * @MongoDB\Document
  */
-class CategoryTranslation extends AbstractPersonalTranslation
+class CategoryTranslation extends AbstractTranslation
 {
     /**
      * Convinient constructor
@@ -25,7 +25,179 @@ class CategoryTranslation extends AbstractPersonalTranslation
     }
 
     /**
+     * @MongoDB\Id
+     */
+    protected $id;
+
+    /**
      * @MongoDB\ReferenceOne(targetDocument="Category", inversedBy="translations")
      */
     protected $object;
+    /**
+     * @var string $locale
+     */
+    protected $locale;
+
+    /**
+     * @var string $objectClass
+     */
+    protected $objectClass;
+
+    /**
+     * @var string $field
+     */
+    protected $field;
+
+    /**
+     * @var string $foreignKey
+     */
+    protected $foreignKey;
+
+    /**
+     * @var string $content
+     */
+    protected $content;
+
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set object
+     *
+     * @param Mongo\ExampleBundle\Document\Category $object
+     * @return self
+     */
+    public function setObject(\Mongo\ExampleBundle\Document\Category $object)
+    {
+        $this->object = $object;
+        return $this;
+    }
+
+    /**
+     * Get object
+     *
+     * @return Mongo\ExampleBundle\Document\Category $object
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * Set locale
+     *
+     * @param string $locale
+     * @return self
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    /**
+     * Get locale
+     *
+     * @return string $locale
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Set objectClass
+     *
+     * @param string $objectClass
+     * @return self
+     */
+    public function setObjectClass($objectClass)
+    {
+        $this->objectClass = $objectClass;
+        return $this;
+    }
+
+    /**
+     * Get objectClass
+     *
+     * @return string $objectClass
+     */
+    public function getObjectClass()
+    {
+        return $this->objectClass;
+    }
+
+    /**
+     * Set field
+     *
+     * @param string $field
+     * @return self
+     */
+    public function setField($field)
+    {
+        $this->field = $field;
+        return $this;
+    }
+
+    /**
+     * Get field
+     *
+     * @return string $field
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * Set foreignKey
+     *
+     * @param string $foreignKey
+     * @return self
+     */
+    public function setForeignKey($foreignKey)
+    {
+        $this->foreignKey = $foreignKey;
+        return $this;
+    }
+
+    /**
+     * Get foreignKey
+     *
+     * @return string $foreignKey
+     */
+    public function getForeignKey()
+    {
+        return $this->foreignKey;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return self
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string $content
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
 }
