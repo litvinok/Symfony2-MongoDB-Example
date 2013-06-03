befefcb4990f3300f4083a98d421762cfbc3ee32
Alex Litvinok <litvinok@gmail.com>
2013-06-03 13:21:15 +0300

    update model

--- src/Mongo/ExampleBundle/Document/Category.php
+++ src/Mongo/ExampleBundle/Document/Category.php
@@ -97,11 +97,7 @@ class Category
     private $updatedBy;
 
     /**
-     * @MongoDB\ReferenceMany(
-     * targetDocument="CategoryTranslation",
-     * mappedBy="object",
-     * cascade={"persist", "remove"}
-     * )
+     * @MongoDB\ReferenceMany( targetDocument="CategoryTranslation", mappedBy="object", cascade={"persist", "remove"} )
      */
     private $translations;
 
@@ -109,108 +105,5 @@ class Category
     {
         $this->children = new ArrayCollection();
         $this->translations = new ArrayCollection();
-    }
-
-    public function getTranslations()
-    {
-        return $this->translations;
-    }
-
-    public function addTranslation(CategoryTranslation $t)
-    {
-        if (!$this->translations->contains($t)) {
-            $this->translations[] = $t;
-            $t->setObject($this);
-        }
-    }
-
-    public function getSlug()
-    {
-        return $this->slug;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    }
-
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    public function setDescription($description)
-    {
-        $this->description = $description;
-    }
-
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    public function setParent($parent)
-    {
-        $this->parent = $parent;
-    }
-
-    public function getParent()
-    {
-        return $this->parent;
-    }
-
-    public function getRoot()
-    {
-        return $this->root;
-    }
-
-    public function getLevel()
-    {
-        return $this->level;
-    }
-
-    public function getChildren()
-    {
-        return $this->children;
-    }
-
-    public function getLeft()
-    {
-        return $this->lft;
-    }
-
-    public function getRight()
-    {
-        return $this->rgt;
-    }
-
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    public function getUpdated()
-    {
-        return $this->updated;
-    }
-
-    public function getCreatedBy()
-    {
-        return $this->createdBy;
-    }
-
-    public function getUpdatedBy()
-    {
-        return $this->updatedBy;
-    }
-
-    public function __toString()
-    {
-        return $this->getTitle();
     }
 }
